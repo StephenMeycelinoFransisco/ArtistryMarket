4bd0834d164b9ca5f4edd2779a1f720cb490f213
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2023-10-18 05:27:26 +0700

    build: slicing artist page

--- src/components/Common/Additional/Additional.jsx
+++ src/components/Common/Additional/Additional.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default function Additional() {
+export default function Additional({number1, number2, number3, caption1, caption2, caption3}) {
   return (
     <div className="grid grid-cols-3 gap-[1.875rem] text-white">
       <div className="">
-        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">240k+</h1>
-        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">Total Sale</caption>
+        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">{number1}</h1>
+        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">{caption1}</caption>
       </div>
       <div className="">
-        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">100k+</h1>
-        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">Auctions</caption>
+        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">{number2}</h1>
+        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">{caption2}</caption>
       </div>
       <div className="">
-        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">240k+</h1>
-        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">Artist</caption>
+        <h1 className="text-[1.375rem] lg:text-[1.75rem] font-bold">{number3}</h1>
+        <caption className="text-[1rem] lg:text-[1.5rem] font-workSans flex">{caption3}</caption>
       </div>
     </div>
   );
