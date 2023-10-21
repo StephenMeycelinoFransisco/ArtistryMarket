0cb59398a547cb9b6b2610c3b4318e9ecdd49e0e
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2023-10-22 00:32:41 +0700

    style: component layout and hero with className grid

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Header from '../components/Fragments/Header/Header'
-import Footer from '../components/Fragments/Footer/Footer'
-import Routes from '../routes/Router'
+import React from 'react';
+import Header from '../components/Fragments/Header/Header';
+import Footer from '../components/Fragments/Footer/Footer';
+import Routes from '../routes/Router';
 
 export default function Layout() {
   return (
     <>
       <Header />
       <section className="grid">
-        <div className="">
+        <div className="grid">
           <Routes />
         </div>
         <div className="bg-black-secondary">
-          <div className="max-w-[17.5rem] mx-auto  lg:max-w-2xl xl:max-w-5xl">
+          <div className="max-w-[17.5rem] mx-auto lg:max-w-2xl xl:max-w-5xl">
             <Footer />
           </div>
         </div>
@@ -20,4 +20,3 @@ export default function Layout() {
     </>
   );
 }
-
